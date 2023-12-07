1086d74b3d45f5093d246739cccbd2160ed35989
jdboachie <judeboachiekwame@gmail.com>
2023-12-07 16:17:40 +0000

    dev update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import CourseCard from '@/components/CourseCard'
+import Search from '@/components/Search'
 import {
     ArchiveBoxIcon
 } from '@heroicons/react/24/outline'
@@ -9,20 +11,35 @@ import {
 import Link from 'next/link'
 
 
+
 const Home = () => {
   return (
-    <main
-        className="py-20 flex flex-col items-center justify-center w-full flex-1 p-0 text-center space-y-5"
+    <div
+        className="flex flex-col items-center justify-center w-full flex-1 text-center space-y-5"
     >
         <nav
-            className='fixed top-0 flex backdrop-blur justify-between w-full p-3 border-b'
+            className='fixed top-0 flex backdrop-blur justify-between w-full p-3 border-b dark:border-b-[#333]'
         >
-            <h1 className="text-lg font-semibold">
+            <h1 className="text-lg font-semibold dark:text-[#eee]">
                 CBET
             </h1>
         </nav>
 
-        <div className='grid grid-cols-4'>
+        <main
+            className='w-full p-5 pt-20'
+        >
+            <p>Courses</p>
+            <div className="grid grid-cols-1 max-sm:grid-cols-1 max-md:grid-cols-2">
+                <CourseCard />
+                <CourseCard />
+                <CourseCard />
+                <CourseCard />
+            </div>
+
+        </main>
+
+
+        {/* <div className='grid grid-cols-4'>
             <div className='p-1 cursor-pointer'>
                 <BookmarkIcon className=' w-[7em] h-[7em] p-5 rounded-lg border text-blue-700 hover:bg-gradient-to-b hover:from-transparent hover:via-transparent hover:to-zinc-300'/>
                 <p className='flex mt-1 px-2 text-sm'>
@@ -34,11 +51,11 @@ const Home = () => {
                 <ClockIcon className=' w-[7em] h-[7em] p-5 rounded-lg border text-blue-700 hover:bg-gradient-to-b hover:from-transparent hover:via-transparent hover:to-zinc-300'/>
                 <p className='flex mt-1 px-2 text-sm'>
                     Recent Files
-                </p>
+                 </p>
                 <p className='text-xs text-gray-500 px-2 flex'>23 files</p>
             </div>
-        </div>
-    </main>
+        </div> */}
+    </div>
   )
 }
 
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,18 +1,18 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid"
 
 
-const Search = () => {
+const Search = ({classNames} : {classNames?: string}) => {
   return (
-    <div className='border rounded-full flex p-1'>
+    <div className={`${classNames} border dark:border-[#333] bg-white dark:bg-black rounded-full flex p-2`}>
         <MagnifyingGlassIcon
             className='w-10 px-2 text-gray-500'
         />
         <input
-            type="text"
-            name=""
-            id=""
-            className='border-none active:border-none focus:border-none outline-none w-full py-1.5 pr-1 text-xs text-gray-600 bg-transparent'
-            />
+          type="text"
+          name=""
+          id=""
+          className='border-none active:border-none focus:border-none outline-none w-full py-1.5 pr-1 text-xs text-gray-600 bg-transparent'
+        />
     </div>
   )
 }
