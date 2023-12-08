84bd22710177f34e82949fd324c627fed4782c7f
jdboachie <judeboachiekwame@gmail.com>
2023-12-08 10:58:38 +0000

    dev update

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Tag = ({tag}: {tag?:string}) => {
   return (
     <div
-        className='border dark:text-[#eee] rounded-full w-fit py-1.5 px-2.5 text-xs'
+        className='border dark:border-[#333] dark:text-[#eee] rounded-full w-fit py-1.5 px-2.5 text-xs'
     >
         {/*This will contain a link to a page that will show all courses and files with the tag*/}
         {tag}
