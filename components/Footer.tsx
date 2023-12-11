0d88a243e101b174a54123ea935d83d7f20e4205
jdboachie <judeboachiekwame@gmail.com>
2023-12-11 16:09:15 +0000

    dev update- settings

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer
-    className='w-full min-h-[3rem] bg-transparent flex justify-center items-center border-t text-xs dark:border-[#333] dark:text-[#eee]'
+    className='w-full min-h-[3rem] bg-transparent flex justify-center items-center border-t text-[0.55em] font-mono dark:border-[#333] dark:text-[#eee]'
     >
         built with ❤️ by Jude
     </footer>
