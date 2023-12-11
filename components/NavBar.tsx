0a1252a306cd5e91902c4884a68f1b9493a62802
jdboachie <judeboachiekwame@gmail.com>
2023-12-11 12:33:12 +0000

    dev update working on dark mode

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -23,15 +23,15 @@ const NavBar = () => {
   return (
       <>
         <nav
-            className='fixed top-0 z-50 flex flex-col justify-between w-full dark:border-b-[#333]'
+            className='fixed top-0 z-50 flex flex-col justify-between w-full dark:border-b-[#333] dark:text-[#eee] bg-white dark:bg-black'
         >
-            <div className='flex w-full justify-between bg-white p-4 '>
+            <div className='flex w-full justify-between bg-white dark:bg-black p-4 '>
                 <div className='flex justify-between space-x-1 w-full'>
                     <div className='space-x-1 flex text-xs text-left text-gray-700'>
                         <div className="w-9 h-9 p-2 animate-shimmer border bg-gray-900 rounded-full">
                             <UserIcon className="text-gray-300" />
                         </div>
-                        <p className='p-0.5 w-fit'>
+                        <p className='p-0.5 w-fit dark:text-[#eee]'>
                             Jude Boachie <br/>
                             judeboachiekwame@gmail.com
                         </p>
@@ -58,22 +58,22 @@ const NavBar = () => {
                 <ul className='flex space-x-1'>
                 <Link
                         href={'/'}
-                        className={`${currentPath === '/' && 'bg-gray-100'} p-3 rounded-lg text-xs hover:bg-gray-100 dark:hover:bg-[#111] w-full`}>
+                        className={`${currentPath === '/' && 'bg-gray-100 dark:bg-blue-800'} p-3 rounded-lg text-xs hover:bg-gray-100 dark:hover:bg-[#111] w-full`}>
                         Courses
                     </Link>
                     <Link
                         href={'/account'}
-                        className={`${currentPath === '/account' && 'bg-gray-100'} p-3 rounded-lg text-xs hover:bg-gray-100 dark:hover:bg-[#111] w-full`}>
+                        className={`${currentPath === '/account' && 'bg-gray-100 dark:bg-blue-800'} p-3 rounded-lg text-xs hover:bg-gray-100 dark:hover:bg-[#111] w-full`}>
                         Account
                     </Link>
                     <Link
                         href={'/settings'}
-                        className={`${currentPath === '/settings' && 'bg-gray-100'} p-3 rounded-lg text-xs hover:bg-gray-100 dark:hover:bg-[#111] w-full`}>
+                        className={`${currentPath === '/settings' && 'bg-gray-100 dark:bg-blue-800'} p-3 rounded-lg text-xs hover:bg-gray-100 dark:hover:bg-[#111] w-full`}>
                         Settings
                     </Link>
                     <Link
                         href={'/logout'}
-                        className={`${currentPath === '/logout' && 'bg-gray-100'} p-3 rounded-lg text-xs hover:bg-gray-100 dark:hover:bg-[#111] w-full`}>
+                        className={`${currentPath === '/logout' && 'bg-gray-100 dark:bg-blue-800'} p-3 rounded-lg text-xs hover:bg-gray-100 dark:hover:bg-[#111] w-full`}>
                         Logout
                     </Link>
                 </ul>
