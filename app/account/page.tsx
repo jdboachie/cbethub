2f987e8f8bba0cade6aefa4c37e69b19fad5a67a
jdboachie <judeboachiekwame@gmail.com>
2023-12-15 21:15:56 +0000

    experimental

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -31,7 +31,7 @@ const page = () => {
                 className='rounded-full'
               />
               {/* <UserIcon className="z-0 p-5 text-gray-300 animate-pulse border dark:border-[#333] rounded-full bg-white dark:bg-[#111]" /> */}
-              <CameraIcon className="z-10 w-9 h-9 p-2 absolute top-[8rem] right-[1rem] transform translate-x-1/3 -translate-y-1/3 rounded-full border text-gray-300 bg-white dark:bg-[#111]" />
+              <CameraIcon className="z-10 w-9 h-9 p-2 absolute top-[8rem] right-[1rem] transform translate-x-1/3 -translate-y-1/3 rounded-full border dark:border-[#333] text-gray-300 bg-white dark:bg-[#111]" />
             </div>
           </div>
           {/** */}
