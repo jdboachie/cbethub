ee3f692c4b59afbca9fb3eae5ed03be265d5ae2a
jdboachie <judeboachiekwame@gmail.com>
2023-12-19 18:41:01 +0000

    working on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,16 @@ const Home = () => {
         <div
             className={`${schibstedGrotesk.className} flex flex-col justify-center items-center h-full text-center font-extrabold`}
         >
-            <p className="text-7xl text-transparent bg-gradient-to-r bg-clip-text from-[#333] via-[#222] dark:from-zinc-400 dark:via-slate-500 to-black dark:to-white">
+            <p className="text-7xl max-sm:text-5xl text-transparent bg-gradient-to-r bg-clip-text from-[#333] via-[#222] dark:from-zinc-400 dark:via-slate-500 to-black dark:to-white">
                 Welcome to the
             </p>
-            <span className={`text-7xl pb-12 text-transparent bg-gradient-to-r from-orange-500 via-red-700 to-blue-600 bg-clip-text`}>
+            <span className={`text-7xl max-sm:text-5xl pb-12 text-transparent bg-gradient-to-r from-orange-500 via-red-700 to-blue-600 bg-clip-text`}>
                 CBET HUB
             </span>
-            <p className="text-xl pb-10 font-semibold text-transparent bg-gradient-to-r bg-clip-text from-[#333] via-[#222] dark:from-zinc-400 dark:via-slate-500 to-black dark:to-white">
+            <p className="text-xl max-sm:text-sm pb-10 font-semibold text-transparent bg-gradient-to-r bg-clip-text from-[#333] via-[#222] dark:from-zinc-400 dark:via-slate-500 to-black dark:to-white">
                 Your one stop shop for all things CBET
             </p>
-            <div className="flex font-normal space-x-2 w-1/3">
+            <div className="flex font-normal space-x-2 max-sm:w-5/6">
                 <Link
                     href='/courses'
                     className="px-5 py-2 w-full text-lg text-center text-black dark:text-white bg-gradient-to-r from-orange-500/20 via-red-700/20 to-blue-600/20 rounded-2xl"
