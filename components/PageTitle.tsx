2eb1a0088a3160358ebef8a53748116467245dca
Jude Boachie <judeboachiekwame@gmail.com>
2024-01-07 22:22:53 +0000

    Update PageTitle.tsx

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const PageTitle = ({title}: {title?: string}) => {
   return (
     <div
-        className={`text-5xl max-sm:text-4xl text-left font-[500] text-gray-700 dark:text-gray-50
-                   px-2 py-7`}
+        className={`text-3xl max-sm:text-2xl text-left font-[500] text-gray-700 dark:text-gray-50
+                   px-2 py-5`}
     >
         {title? title : 'Error'}
     </div>
