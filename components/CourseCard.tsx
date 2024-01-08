8599b78629c56bc5f82b6109eec75f9c7eed255e
Jude Boachie <judeboachiekwame@gmail.com>
2024-01-08 18:15:16 +0000

    Update CourseCard.tsx

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -37,7 +37,7 @@ const CourseCard = (
             {lecturerName}
         </p>
         <p
-            className={`text-left pb-1 text-sm max-sm:text-sm font-[500] text-gray-700 dark:text-gray-200`}
+            className={`text-left pb-1 max-md:text-xs text-sm font-extrabold text-gray-700 dark:text-gray-200`}
         >
             {courseName}
         </p>
